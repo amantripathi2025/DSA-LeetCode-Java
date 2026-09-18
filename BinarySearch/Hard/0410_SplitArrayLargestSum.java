8d22274ce4853683de39b082767620cdc2c4605b
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-09-18 19:44:12 +0530

    feat: add LeetCode 410 solution

--- BinarySearch/Hard/0410_SplitArrayLargestSum.java
+++ BinarySearch/Hard/0410_SplitArrayLargestSum.java
@@ -1,1 +1,44 @@
+package BinarySearch;
+//HARD Asked in Google
+public class Leetcode410Split {
+    static void main() {
+
+    }
+    public int splitArray(int[]nums,int m){
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i< nums.length; i++){
+            start = Math.max(start,nums[i]); //in the end of loop this will contain max of the array
+            end += nums[i];
+        }
+        //binary search
+        while(start < end){
+            //try for the middle as potential answer
+            int mid = start + (end - start)/2;
+
+
+            //calculate how many pices you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums){
+                if(sum + num > mid){
+                    //you cannot add this in subarray make new one
+                    //say you add this num in new subarray then, sum = num
+                    sum = num;
+                     pieces++;
+                }
+                else{
+                    sum += num;
+                }
+            }
+            if(pieces > m){
+                start = mid+1;
+            } else{
+               end = mid;
+            }
+        }
+        return end;
+    }
+}
 
