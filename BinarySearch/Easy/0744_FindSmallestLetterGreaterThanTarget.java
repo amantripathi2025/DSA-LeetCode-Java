335b9846a318b5f7854297d2e66448db01bebcca
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-09-24 21:35:02 +0530

    Update 0744_FindSmallestLetterGreaterThanTarget.java

--- BinarySearch/Easy/0744_FindSmallestLetterGreaterThanTarget.java
+++ BinarySearch/Easy/0744_FindSmallestLetterGreaterThanTarget.java
@@ -1,1 +1,27 @@
+public class leetcode744 {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'j';
 
+        char ans = binarySearch(letters, target);
+        System.out.println(ans);
+        System.out.println("The smallest letter greater than target is " + ans);
+    }
+
+    static char binarySearch(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % letters.length];
+    }
+}
